78be9a81943abef86cd9cdcb9ef54cd02a7055ed
Jan Blaakmeer <jan@blaakmeer.nl>
2020-04-28 07:42:46 +0200

    Initial Commit

---
+++ TestGIT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestGIT
+//
+//  Created by Jan Blaakmeer on 28/04/2020.
+//  Copyright © 2020 Jan Blaakmeer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
